223f1946722d4156269d196e6af4b699a1a143ef
Gennadii Saltyshchak <GennadyGS@gmail.com>
2016-04-25 10:41:04 +0300

    Refactoring

--- TplDataFlow.Extensions/ForkBlock.cs
+++ TplDataFlow.Extensions/ForkBlock.cs
@@ -4,40 +4,40 @@ using System.Threading.Tasks.Dataflow;
 
 namespace TplDataFlow.Extensions
 {
-    public class ForkBlock<TInput, TOutputLeft, TOutputRight> : ITargetBlock<TInput>
+    public class ForkBlock<TInput, TOutput1, TOutput2> : ITargetBlock<TInput>
     {
         private readonly ITargetBlock<TInput> _input;
 
-        private readonly IPropagatorBlock<TOutputLeft, TOutputLeft> _leftOutput = new BufferBlock<TOutputLeft>();
-        private readonly IPropagatorBlock<TOutputRight, TOutputRight> _rightOutput = new BufferBlock<TOutputRight>();
+        private readonly IPropagatorBlock<TOutput1, TOutput1> _output1 = new BufferBlock<TOutput1>();
+        private readonly IPropagatorBlock<TOutput2, TOutput2> _output2 = new BufferBlock<TOutput2>();
 
-        public ForkBlock(Func<TInput, Task<Tuple<TOutputLeft, TOutputRight>>> transform)
+        public ForkBlock(Func<TInput, Task<Tuple<TOutput1, TOutput2>>> transform)
         {
             _input = new ActionBlock<TInput>(CreateTransformActionAsync(transform));
 
             PropagateCompletion();
         }
 
-        public ForkBlock(Func<TInput, Tuple<TOutputLeft, TOutputRight>> transform)
+        public ForkBlock(Func<TInput, Tuple<TOutput1, TOutput2>> transform)
         {
             _input = new ActionBlock<TInput>(CreateTransformActionSync(transform));
 
             PropagateCompletion();
         }
 
-        public ISourceBlock<TOutputLeft> LeftOutput
+        public ISourceBlock<TOutput1> Output1
         {
             get
             {
-                return _leftOutput;
+                return _output1;
             }
         }
 
-        public ISourceBlock<TOutputRight> RightOutput
+        public ISourceBlock<TOutput2> Output2
         {
             get
             {
-                return _rightOutput;
+                return _output2;
             }
         }
 
@@ -60,43 +60,43 @@ namespace TplDataFlow.Extensions
         {
             get
             {
-                return Task.WhenAll(_leftOutput.Completion, _rightOutput.Completion);
+                return Task.WhenAll(_output1.Completion, _output2.Completion);
             }
         }
 
-        public ITargetBlock<TInput> ForkTo(ITargetBlock<TOutputLeft> targetLeft, ITargetBlock<TOutputRight> targetRight)
+        public ITargetBlock<TInput> ForkTo(ITargetBlock<TOutput1> target1, ITargetBlock<TOutput2> target2)
         {
-            LeftOutput.LinkWith(targetLeft);
-            RightOutput.LinkWith(targetRight);
+            Output1.LinkWith(target1);
+            Output2.LinkWith(target2);
             return this;
         }
 
-        private Action<TInput> CreateTransformActionSync(Func<TInput, Tuple<TOutputLeft, TOutputRight>> transformFunc)
+        private Action<TInput> CreateTransformActionSync(Func<TInput, Tuple<TOutput1, TOutput2>> transformFunc)
         {
             return value => PropagateResult(transformFunc(value));
         }
 
-        private Func<TInput, Task> CreateTransformActionAsync(Func<TInput, Task<Tuple<TOutputLeft, TOutputRight>>> transformFunc)
+        private Func<TInput, Task> CreateTransformActionAsync(Func<TInput, Task<Tuple<TOutput1, TOutput2>>> transformFunc)
         {
             return async value => await PropagateResultAsync(await transformFunc(value));
         }
 
-        private void PropagateResult(Tuple<TOutputLeft, TOutputRight> result)
+        private void PropagateResult(Tuple<TOutput1, TOutput2> result)
         {
-            _leftOutput.Post(result.Item1);
-            _rightOutput.Post(result.Item2);
+            _output1.Post(result.Item1);
+            _output2.Post(result.Item2);
         }
 
-        private async Task PropagateResultAsync(Tuple<TOutputLeft, TOutputRight> result)
+        private async Task PropagateResultAsync(Tuple<TOutput1, TOutput2> result)
         {
             await Task.WhenAll(
-                _leftOutput.SendAsync(result.Item1),
-                _rightOutput.SendAsync(result.Item2));
+                _output1.SendAsync(result.Item1),
+                _output2.SendAsync(result.Item2));
         }
 
         private void PropagateCompletion()
         {
-            _input.PropagateCompletion(_leftOutput, _rightOutput);
+            _input.PropagateCompletion(_output1, _output2);
         }
     }
 }
