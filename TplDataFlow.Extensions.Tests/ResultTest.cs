bb3d782f8969b80151f4ee3dd62d343f9477e8f0
Gennadii Saltyshchak <GennadyGS@gmail.com>
2016-05-12 11:06:35 +0300

    Result type added

---
+++ TplDataFlow.Extensions.Tests/ResultTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace TplDataFlow.Extensions.UnitTests
+{
+    public class ResultTest
+    {
+        [Fact]
+        public void Result_ShouldBeSuccess()
+        {
+            const int input = 1;
+
+            var result = 
+                from i in Result.Success<int, string>(input)
+                select i + 1;
+
+            result.IsSuccess
+                .Should().BeTrue();
+            result.Success
+                .Should().Be(input + 1);
+            Assert.Throws<InvalidOperationException>(() => result.Failure);
+        }
+        [Fact]
+
+        public void Result_ShouldBeFailure()
+        {
+            const int input = 1;
+            const string failure = "failure";
+
+            var result = 
+                from i in Result.Success<int, string>(input)
+                select Result.Failure<int, string>(failure);
+
+            result.IsSuccess
+                .Should().BeFalse();
+            result.Failure
+                .Should().Be(failure);
+            Assert.Throws<InvalidOperationException>(() => result.Success);
+        }
+    }
+}
---
+++ TplDataFlow.Extensions/Result.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace TplDataFlow.Extensions
+{
+    public class Result<TSuccess, TFailure>
+    {
+        private readonly TSuccess _success;
+        private readonly TFailure _failure;
+
+        internal Result(TSuccess success)
+        {
+            _success = success;
+            _failure = default(TFailure);
+            IsSuccess = true;
+        }
+
+        internal Result(TFailure failure)
+        {
+            _success = default(TSuccess);
+            _failure = failure;
+            IsSuccess = false;
+        }
+
+        public TSuccess Success
+        {
+            get
+            {
+                if (!IsSuccess)
+                {
+                    throw new InvalidOperationException("Result is not success");
+                }
+                return _success;
+            }
+        }
+
+        public TFailure Failure
+        {
+            get
+            {
+                if (IsSuccess)
+                {
+                    throw new InvalidOperationException("Result is not failure");
+                }
+                return _failure;
+            }
+        }
+
+        public bool IsSuccess { get; }
+
+        public static implicit operator Result<TSuccess, TFailure>(TSuccess success)
+        {
+            return new Result<TSuccess, TFailure>(success);
+        }
+
+        public static implicit operator Result<TSuccess, TFailure>(TFailure failure)
+        {
+            return new Result<TSuccess, TFailure>(failure);
+        }
+    }
+
+    public static class Result
+    {
+        public static Result<TSuccess, TFailure> Success<TSuccess, TFailure>(TSuccess success)
+        {
+            return new Result<TSuccess, TFailure>(success);
+        }
+
+        public static Result<TSuccess, TFailure> Failure<TSuccess, TFailure>(TFailure failure)
+        {
+            return new Result<TSuccess, TFailure>(failure);
+        }
+
+        public static T Match<TSuccess, TFailure, T>(this Result<TSuccess, TFailure> self, Func<TSuccess, T> onSuccess, Func<TFailure, T> onFailure)
+        {
+            return self.IsSuccess
+                ? onSuccess(self.Success)
+                : onFailure(self.Failure);
+        }
+
+        public static Result<TOutput, TFailure> Select<TInput, TOutput, TFailure>(this Result<TInput, TFailure> self, Func<TInput, TOutput> selector)
+        {
+            if (!self.IsSuccess)
+            {
+                return Failure<TOutput, TFailure>(self.Failure);
+            }
+            return Success<TOutput, TFailure>(selector(self.Success));
+        }
+
+        public static Result<TOutput, TFailure> Select<TInput, TOutput, TFailure>(this Result<TInput, TFailure> self, Func<TInput, Result<TOutput, TFailure>> selector)
+        {
+            if (!self.IsSuccess)
+            {
+                return Failure<TOutput, TFailure>(self.Failure);
+            }
+            return selector(self.Success);
+        }
+
+        public static Result<TOutput, TFailure> SelectMany<TInput, TOutput, TFailure>(this Result<TInput, TFailure> self, Func<TInput, Result<TOutput, TFailure>> selector)
+        {
+            if (!self.IsSuccess)
+            {
+                return Failure<TOutput, TFailure>(self.Failure);
+            }
+            return selector(self.Success);
+        }
+    }
+}
