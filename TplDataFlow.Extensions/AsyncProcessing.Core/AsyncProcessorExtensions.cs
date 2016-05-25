b44745b7d54b716da96d46c60f492866b5fa3c89
Gennadii Saltyshchak <GennadyGS@gmail.com>
2016-05-25 18:42:27 +0300

    Fix race conditions in AsyncProcessorExtensions

--- TplDataFlow.Extensions/AsyncProcessing.Core/AsyncProcessorExtensions.cs
+++ TplDataFlow.Extensions/AsyncProcessing.Core/AsyncProcessorExtensions.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Reactive.Linq;
+using System.Threading.Tasks;
 
 namespace TplDataFlow.Extensions.AsyncProcessing.Core
 {
@@ -7,8 +9,25 @@ namespace TplDataFlow.Extensions.AsyncProcessing.Core
     {
         public static IList<TOutput> InvokeSync<TInput, TOutput>(this IAsyncProcessor<TInput, TOutput> processor, IList<TInput> input)
         {
+            return InvokeAsync(processor, input).Result;
+        }
+
+        private static Task<IList<TOutput>> InvokeAsync<TInput, TOutput>(IAsyncProcessor<TInput, TOutput> processor, IList<TInput> input)
+        {
+            var result = processor.ToListAsync();
             input.ToObservable().Subscribe(processor);
-            return processor.ToList().GetAwaiter().GetResult();
+            return result;
+        }
+
+        private static Task<IList<T>> ToListAsync<T>(this IObservable<T> observable)
+        {
+            var taskSource = new TaskCompletionSource<IList<T>>();
+            var result = new List<T>();
+            observable.Subscribe(
+                onNext: result.Add,
+                onError: taskSource.SetException,
+                onCompleted: () => taskSource.SetResult(result));
+            return taskSource.Task;
         }
     }
 }
