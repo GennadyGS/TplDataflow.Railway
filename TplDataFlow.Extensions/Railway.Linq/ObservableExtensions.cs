a1b805320c89d0819af4d71874747ed6a4d5d375
Gennadii Saltyshchak <GennadyGS@gmail.com>
2016-05-26 10:58:41 +0300

    Implemented new methods class in Railway.Linq.ObservableExtensions

--- TplDataFlow.Extensions/Railway.Linq/ObservableExtensions.cs
+++ TplDataFlow.Extensions/Railway.Linq/ObservableExtensions.cs
@@ -9,11 +9,82 @@ namespace TplDataFlow.Extensions.Railway.Linq
 {
     public static class ObservableExtensions
     {
+        public static IObservable<Either<TLeft, TRightOutput>> Select<TLeft, TRightInput, TRightOutput>(
+            this IObservable<Either<TLeft, TRightInput>> source,
+            Func<TRightInput, TRightOutput> selector)
+        {
+            return source.Select(item => item.Map(selector));
+        }
+
+        public static IObservable<Either<TLeft, TRightOutput>> SelectMany<TLeft, TRightInput, TRightOutput>(
+            this IObservable<Either<TLeft, TRightInput>> source,
+            Func<TRightInput, IEnumerable<TRightOutput>> selector)
+        {
+            return source.SelectMany(item => item.SelectMany(selector));
+        }
+
+        public static IObservable<Either<TLeft, TRightOutput>> SelectMany
+            <TLeft, TRightInput, TRightMedium, TRightOutput>(
+            this IObservable<Either<TLeft, TRightInput>> source,
+            Func<TRightInput, IEnumerable<TRightMedium>> mediumSelector,
+            Func<TRightInput, TRightMedium, TRightOutput> resultSelector)
+        {
+            return source.SelectMany(item => item.SelectMany(mediumSelector, resultSelector));
+        }
+
+        public static IObservable<Either<TLeft, TRightOutput>> SelectSafe<TLeft, TRightInput, TRightOutput>(
+            this IObservable<Either<TLeft, TRightInput>> source,
+            Func<TRightInput, Either<TLeft, TRightOutput>> selector)
+        {
+            return source.Select(item => item.SelectSafe(selector));
+        }
+
+        public static IObservable<Either<TLeft, TRightOutput>> SelectSafe
+            <TLeft, TRightInput, TRightMedium, TRightOutput>(
+            this IObservable<Either<TLeft, TRightInput>> source,
+            Func<TRightInput, Either<TLeft, TRightMedium>> mediumSelector,
+            Func<TRightInput, TRightMedium, TRightOutput> resultSelector)
+        {
+            return source.Select(item => item.SelectSafe(mediumSelector, resultSelector));
+        }
+
+        public static IObservable<Either<TLeft, TRightOutput>> SelectManySafe<TLeft, TRightInput, TRightOutput>(
+            this IObservable<Either<TLeft, TRightInput>> source,
+            Func<TRightInput, IEnumerable<Either<TLeft, TRightOutput>>> selector)
+        {
+            return source.SelectMany(item => item.SelectManySafe(selector));
+        }
+
+        public static IObservable<Either<TLeft, TRightOutput>> SelectManySafe
+            <TLeft, TRightInput, TRightMedium, TRightOutput>(
+            this IObservable<Either<TLeft, TRightInput>> source,
+            Func<TRightInput, IEnumerable<Either<TLeft, TRightMedium>>> mediumSelector,
+            Func<TRightInput, TRightMedium, TRightOutput> resultSelector)
+        {
+            return source.SelectMany(item => item.SelectManySafe(mediumSelector, resultSelector));
+        }
+
+        public static IObservable<Either<TLeft, IGroupedObservable<TKey, TRight>>> GroupBy<TLeft, TRight, TKey>(
+            this IObservable<Either<TLeft, TRight>> source, Func<TRight, TKey> keySelector)
+        {
+            return source
+                .GroupBy(item => item.IsRight)
+                .SelectMany(
+                    group => group.Key
+                        ? group
+                            .SelectMany(item => item.RightAsEnumerable())
+                            .GroupBy(keySelector)
+                            .Select(Right<TLeft, IGroupedObservable<TKey, TRight>>)
+                        : group
+                            .SelectMany(item => item.LeftAsEnumerable())
+                            .Select(Left<TLeft, IGroupedObservable<TKey, TRight>>));
+        }
+
         public static IObservable<Either<TLeft, IList<TRight>>> BufferSafe<TLeft, TRight>(
             this IObservable<Either<TLeft, TRight>> source, TimeSpan timeSpan, int count)
         {
             return source
-                .Buffer(count)
+                .Buffer(timeSpan, count)
                 .SelectMany(batch => batch
                     .GroupBy(item => item.IsRight)
                     .SelectMany(group => group.Key
