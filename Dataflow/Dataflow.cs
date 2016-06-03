3a3515121af6045157c78ae9e4e2dff89d4f293b
Gennadii Saltyshchak <GennadyGS@gmail.com>
2016-06-03 17:38:34 +0300

    Dataflow assembly added

---
+++ Dataflow/Dataflow.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Dataflow
+{
+    public abstract class Dataflow<TInput, TOutput>
+    {
+    }
+
+    public class Return<TInput, TOutput> : Dataflow<TInput, TOutput>
+    {
+        public Return(TOutput result)
+        {
+            Result = result;
+        }
+
+        public TOutput Result { get; }
+    }
+
+    public class Continuation<TInput, TOutput> : Dataflow<TInput, TOutput>
+    {
+        public Continuation(Func<TInput, Dataflow<TInput, TOutput>> continuationFunc)
+        {
+            ContinuationFunc = continuationFunc;
+        }
+
+        public Func<TInput, Dataflow<TInput, TOutput>> ContinuationFunc { get; }
+    }
+}
