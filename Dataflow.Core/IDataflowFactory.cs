92413b45cb623641bb8bb323adae489fea8cc557
Gennadii Saltyshchak <GennadyGS@gmail.com>
2016-07-04 13:05:53 +0300

    Add IDataflowFactory interface

---
+++ Dataflow.Core/IDataflowFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dataflow.Core
+{
+    interface IDataflowFactory
+    {
+        Dataflow<TOutput> Calculation<TInput, TOutput>(DataflowOperator<TInput> @operator,
+            Func<TInput, Dataflow<TOutput>> continuation);
+
+        Return<T> Return<T>(T value);
+
+        ReturnMany<T> ReturnMany<T>(IEnumerable<T> value);
+
+        Buffer<T> Buffer<T>(T item, TimeSpan batchTimeout, int batchMaxSize);
+    }
+}
