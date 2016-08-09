e1a56e7570cdf51ca76e89e5319c158d7df0885a
Gennadii Saltyshchak <GennadyGS@gmail.com>
2016-08-09 10:59:02 +0300

    Add project templates Dataflow.FSharp and Dataflow.FSharp.Tests

---
+++ Dataflow.FSharp.Tests/DataflowTests.fs
@@ -0,0 +1,10 @@
+﻿namespace Dataflow.FSharp.Tests
+
+open Xunit
+open Swensen.Unquote
+
+type DataflowTests() = 
+    
+    [<Fact>]
+    let ``Test`` ()  = 
+        test <@ 2 + 2 = 4 @>           
