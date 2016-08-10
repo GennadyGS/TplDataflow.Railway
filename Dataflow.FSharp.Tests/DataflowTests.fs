1c403a4080b7ce38795bcf30516e6b294aca5017
Gennadii Saltyshchak <GennadyGS@gmail.com>
2016-08-10 18:51:13 +0300

    Dataflow.FSharp.Tests: Add first useful test

--- Dataflow.FSharp.Tests/DataflowTests.fs
+++ Dataflow.FSharp.Tests/DataflowTests.fs
@@ -2,9 +2,20 @@
 
 open Xunit
 open Swensen.Unquote
+open Dataflow.Core
 
 type DataflowTests() = 
     
+    let TestBindDataflow (input: seq<'input>) (expectedOutput: seq<'output>) 
+        (dataflowBindFunc: IDataflowFactory -> 'input -> IDataflow<'output>) =
+            let inputList = input |> Seq.toList
+            let expectedOutputList = expectedOutput |> Seq.toList
+            let output = input.BindDataflow(System.Func<_,_,_>(dataflowBindFunc)) |> Seq.toList
+            test <@ expectedOutputList = output @>           
+
     [<Fact>]
-    let ``Test`` ()  = 
-        test <@ 2 + 2 = 4 @>           
+    let ``Bind Return Dataflow Should Return The Same List`` ()  = 
+        let input =  [|1, 2, 3 |];
+        let expectedOutput = input |> Array.copy;
+        TestBindDataflow input expectedOutput (fun dataflowFactory i -> dataflowFactory.Return i)
+          
