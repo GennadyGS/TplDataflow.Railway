ad98350a0e40ec19c700f3d3b481d05ebe947f03
Gennadii Saltyshchak <GennadyGS@gmail.com>
2016-08-13 16:02:00 +0300

    DataflowTests.fs refactoring

--- Dataflow.FSharp.Tests/DataflowTests.fs
+++ Dataflow.FSharp.Tests/DataflowTests.fs
@@ -6,16 +6,14 @@ open Dataflow.Core
 
 type DataflowTests() = 
     
-    let TestBindDataflow (input: seq<'input>) (expectedOutput: seq<'output>) 
+    let testBindDataflow (input: seq<'input>) (expectedOutput: seq<'output>) 
         (dataflowBindFunc: IDataflowFactory -> 'input -> IDataflow<'output>) =
-            let inputList = input |> Seq.toList
-            let expectedOutputList = expectedOutput |> Seq.toList
-            let output = input.BindDataflow(System.Func<_,_,_>(dataflowBindFunc)) |> Seq.toList
-            test <@ expectedOutputList = output @>           
+            let output = input.BindDataflow(System.Func<_,_,_>(dataflowBindFunc))
+            test <@ (expectedOutput |> Seq.toList) = (output |> Seq.toList) @>           
 
     [<Fact>]
     let ``Bind Return Dataflow Should Return The Same List`` ()  = 
-        let input =  [|1, 2, 3 |];
-        let expectedOutput = input |> Array.copy;
-        TestBindDataflow input expectedOutput (fun dataflowFactory i -> dataflowFactory.Return i)
+        let input = [1 .. 3];
+        let expectedOutput = input
+        testBindDataflow input expectedOutput (fun dataflowFactory i -> dataflowFactory.Return i)
           
