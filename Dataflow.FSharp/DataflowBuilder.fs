e7f7c76299f9e1fb167839945746b479a4eb8aa6
Gennadii Saltyshchak <GennadyGS@gmail.com>
2016-08-18 09:14:20 +0300

    Simplify DataflowBuilder

--- Dataflow.FSharp/DataflowBuilder.fs
+++ Dataflow.FSharp/DataflowBuilder.fs
@@ -6,4 +6,4 @@ open System
 type DataflowBuilder(factory : IDataflowFactory) =
     member this.Return(value) = factory.Return value
     member this.ReturnFrom(value) = value
-    member this.Bind<'input, 'output>(comp : 'input IDataflow, func : 'input -> 'output IDataflow) : 'output IDataflow = comp.Bind(Func<_,_>(func))
+    member this.Bind(comp : 'input IDataflow, func : 'input -> 'output IDataflow) = comp.Bind(Func<_,_>(func))
