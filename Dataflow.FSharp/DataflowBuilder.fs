444c090c4cba5895e4160cd8cf9fb3d37f9eec32
Gennadii Saltyshchak <GennadyGS@gmail.com>
2016-08-18 09:12:49 +0300

    Remove Zero method from DataflowBuilder

--- Dataflow.FSharp/DataflowBuilder.fs
+++ Dataflow.FSharp/DataflowBuilder.fs
@@ -7,4 +7,3 @@ type DataflowBuilder(factory : IDataflowFactory) =
     member this.Return(value) = factory.Return value
     member this.ReturnFrom(value) = value
     member this.Bind<'input, 'output>(comp : 'input IDataflow, func : 'input -> 'output IDataflow) : 'output IDataflow = comp.Bind(Func<_,_>(func))
-    member this.Zero() = factory.Return ()
