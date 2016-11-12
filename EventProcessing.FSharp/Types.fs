e9268374777305555bad26a7d44b30f9eb6bf869
Gennadii Saltyshchak <GennadyGS@gmail.com>
2016-11-12 12:48:24 +0200

    Types.fs refactoring

--- EventProcessing.FSharp/Types.fs
+++ EventProcessing.FSharp/Types.fs
@@ -14,32 +14,35 @@ open AsyncProcessing.Dataflow.TplDataflow
 [<AbstractClass>]
 type internal BaseDataflowIndividualAsyncFactory() = 
     inherit EventSetStorageProcessor.FactoryBase()
+    
     override this.InternalCreateStorageProcessor(logic, configuration) = 
         this.CreateDataflowAsyncProcessor(
             Func<_,_,_>(fun dataflowFactory event ->
-                this.ProcessEventDataflow(logic, configuration, dataflowFactory, event)))
+                this.ProcessEventDataflow logic configuration dataflowFactory event))
+    
     abstract member CreateDataflowAsyncProcessor : 
         Func<IDataflowFactory, EventDetails, IDataflow<EventSetStorageProcessor.Result>> -> 
         IAsyncProcessor<EventDetails, EventSetStorageProcessor.Result>
+    
     member private this.ProcessEventDataflow
-        (logic: EventSetStorageProcessor.Logic, configuration: IEventSetConfiguration,
-         dataflowFactory: IDataflowFactory, event: EventDetails)
-        : IDataflow<EventSetStorageProcessor.Result> = 
-        dataflowFactory.Return(event)
-            .Select(logic.LogEvent)
-            .Buffer(configuration.EventBatchTimeout, configuration.EventBatchSize)
-            .SelectManyAsync(fun item -> logic.SplitEventsIntoGroupsSafeAsync(item))
-            .SelectSafe(fun item -> logic.FilterSkippedEventGroup(item))
-            .GroupBySafe(fun group -> group.EventSetType.GetCode())
-            .SelectManySafe(fun innerGroup ->
-                innerGroup
-                    .ToList()
-                    .SelectManyAsync(fun item -> logic.ProcessEventGroupsSafeAsync(item)))
-            .SelectMany(fun (res : Either<EventSetStorageProcessor.UnsuccessResult, EventSetStorageProcessor.SuccessResult>) ->
-                EventSetStorageProcessor.Logic.TransformResult(res))
+        (logic : EventSetStorageProcessor.Logic) (configuration : IEventSetConfiguration)
+        (dataflowFactory : IDataflowFactory) (event : EventDetails) = 
+            dataflowFactory.Return(event)
+                .Select(logic.LogEvent)
+                .Buffer(configuration.EventBatchTimeout, configuration.EventBatchSize)
+                .SelectManyAsync(fun item -> logic.SplitEventsIntoGroupsSafeAsync(item))
+                .SelectSafe(fun item -> logic.FilterSkippedEventGroup(item))
+                .GroupBySafe(fun group -> group.EventSetType.GetCode())
+                .SelectManySafe(fun innerGroup ->
+                    innerGroup
+                        .ToList()
+                        .SelectManyAsync(fun item -> logic.ProcessEventGroupsSafeAsync(item)))
+                .SelectMany(fun (res : Either<EventSetStorageProcessor.UnsuccessResult, EventSetStorageProcessor.SuccessResult>) ->
+                    EventSetStorageProcessor.Logic.TransformResult(res))
 
 type internal ObservableDataflowIndividualAsyncFactory() = 
     inherit BaseDataflowIndividualAsyncFactory()
+    
     override this.CreateDataflowAsyncProcessor
         (bindFunc : Func<IDataflowFactory, EventDetails, IDataflow<EventSetStorageProcessor.Result>>)
         : IAsyncProcessor<EventDetails, EventSetStorageProcessor.Result> =
@@ -48,7 +51,9 @@ type internal ObservableDataflowIndividualAsyncFactory() =
 
 type internal TplDataflowDataflowIndividualAsyncFactory() = 
     inherit BaseDataflowIndividualAsyncFactory()
+    
     override this.CreateDataflowAsyncProcessor
         (bindFunc : Func<IDataflowFactory, EventDetails, IDataflow<EventSetStorageProcessor.Result>>) 
         : IAsyncProcessor<EventDetails, EventSetStorageProcessor.Result> =
-            TplDataflowDataflowAsyncProcessor<EventDetails, EventSetStorageProcessor.Result>(bindFunc) :> IAsyncProcessor<EventDetails, EventSetStorageProcessor.Result>
+            TplDataflowDataflowAsyncProcessor<EventDetails, EventSetStorageProcessor.Result>(bindFunc) 
+                :> IAsyncProcessor<EventDetails, EventSetStorageProcessor.Result>
